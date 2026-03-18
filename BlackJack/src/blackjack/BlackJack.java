e770c8dd87b1789f3bd6f6993afe7ca864a1a715
65701501 <ST10510756@myemeris.edu.za>
2026-03-18 09:22:21 +0200

    Added more properties to the display object
    Made the JFrame visible and set its location null. Also played around with some display sizes

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -1,20 +1,34 @@
 
 package blackjack;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
+
+
+
 public class BlackJack {
+
 
     public static void main(String[] args) {
         
-    JFrame display = new JFrame(); 
-    
-    //Create JFrame frontend display
+    JFrame display = new JFrame(); // JFrame Object 
     
     display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Makes it so the application stops running when closed
-    display.setResizable(false); // Makes it so that the Window cannot be resized
+    display.setResizable(true); // Makes it so that the Window cannot be resized
+    //Temp true statement, will change back after size of display has been set
     display.setTitle("Blackjack");
     
+    display.setLocationRelativeTo(null); // Makes it so the application opens in the middle of the screen
+    display.setVisible(true); // Visualizes the window (Makes it visable)
     
+    display.setSize(750, 500); //( new Dimension( 20 , 20));
+    
+   // Create two buttons Hit and Stand
+   
+   // Connect the Array data to the card data and graphics
+   
+   // Add shuffle feature for the deck of cards
+   
 
 
 
