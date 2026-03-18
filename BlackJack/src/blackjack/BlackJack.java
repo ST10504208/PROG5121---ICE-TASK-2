30ec9f60000e97fb2e5a703e66faa5846d504be0
65701501 <ST10510756@myemeris.edu.za>
2026-03-18 10:21:25 +0200

    Reorganized code + Added First Button
    Put Window display into its own method and created another method for "Hit!" button.

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -1,28 +1,44 @@
 
 package blackjack;
 
-import java.awt.Dimension;
+import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-
-
 
 public class BlackJack {
 
 
-    public static void main(String[] args) {
-        
-    JFrame display = new JFrame(); // JFrame Object 
+public static JFrame windowConfig(){
+            
+    JFrame display = new JFrame();                          // JFrame Object 
     
     display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Makes it so the application stops running when closed
-    display.setResizable(true); // Makes it so that the Window cannot be resized
-    //Temp true statement, will change back after size of display has been set
-    display.setTitle("Blackjack");
+    display.setResizable(false);                            // Makes it so that the Window cannot be resized
+    display.setTitle("Blackjack");                          // Creates the Title of the window
+    display.setSize(900, 675);                              // Size of Window
+    display.setLocationRelativeTo(null);                    // Makes it so the application opens in the middle of the screen
     
-    display.setLocationRelativeTo(null); // Makes it so the application opens in the middle of the screen
-    display.setVisible(true); // Visualizes the window (Makes it visable)
+    return display;
+}
+
+public static void buttonConfig(JFrame display){
+    JButton hitButton = new JButton("Hit!");
+    hitButton.addActionListener(null);
+    display.add(hitButton);
     
-    display.setSize(750, 500); //( new Dimension( 20 , 20));
-    
+    JButton standButton = new JButton("Stand");
+    hitButton.addActionListener(null);
+    display.add(standButton);
+}
+
+    public static void main(String[] args) {
+        JFrame display = windowConfig();
+        buttonConfig(display);
+        buttonConfig(display);
+        display.setVisible(true);                             // Visualizes the window (Makes it visable) 
+        
+        
+        
    // Create two buttons Hit and Stand
    
    // Connect the Array data to the card data and graphics
