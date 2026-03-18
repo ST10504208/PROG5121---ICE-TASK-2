bf568cff842c1f1f0047430ba9ce76be3b2a5c74
65701501 <ST10510756@myemeris.edu.za>
2026-03-18 08:27:51 +0200

    Added JFrame Object
    Created the JFrame object and started writing the properties of the window display.

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package blackjack;
 
-/**
- *
- * @author charl
- */
+import javax.swing.JFrame;
 public class BlackJack {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    JFrame display = new JFrame(); 
+    
+    //Create JFrame frontend display
+    
+    display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Makes it so the application stops running when closed
+    display.setResizable(false); // Makes it so that the Window cannot be resized
+    display.setTitle("Blackjack");
+    
+    
+
+
+
     }
     
 }
