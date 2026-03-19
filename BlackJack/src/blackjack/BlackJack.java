4516402adf96330c8a948a9587c7afee918ab4b9
65701501 <ST10510756@myemeris.edu.za>
2026-03-19 18:02:54 +0200

    Added JPanel
    Added Buttons Hit and Stay aswell as background/board for game and cards to sit on

--- BlackJack/src/blackjack/BlackJack.java
+++ BlackJack/src/blackjack/BlackJack.java
@@ -1,9 +1,11 @@
 
 package blackjack;
 
-import javax.swing.Action;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class BlackJack {
 
@@ -11,31 +13,41 @@ public class BlackJack {
 public static JFrame windowConfig(){
             
     JFrame display = new JFrame();                          // JFrame Object 
+    JPanel gamePanel = new JPanel();                        // Creates gamePanel Object that will fit in the JFrame
     
     display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Makes it so the application stops running when closed
     display.setResizable(false);                            // Makes it so that the Window cannot be resized
     display.setTitle("Blackjack");                          // Creates the Title of the window
     display.setSize(900, 675);                              // Size of Window
+    
     display.setLocationRelativeTo(null);                    // Makes it so the application opens in the middle of the screen
+    gamePanel.setLayout(new BorderLayout());                // Set the Game Panel to fit the JFrame
+    gamePanel.setBackground(new Color(53, 101, 57));        // Set the background color to dark green
+    display.add(gamePanel);                                 // Adds the Game Panel to the JFrame
+    
     
     return display;
 }
 
 public static void buttonConfig(JFrame display){
-    JButton hitButton = new JButton("Hit!");
-    hitButton.addActionListener(null);
-    display.add(hitButton);
+    JButton hitButton = new JButton("Hit!");               // Creates button objects
+    JButton stayButton = new JButton("Stay");   
+    JPanel buttonPanel = new JPanel();                     // Creates JPanel for the Buttons 
+ 
+    hitButton.setFocusable(true);                          // Allows the button to interact with input
+    hitButton.addActionListener(null);                     // Creates output after button is pressed
+    buttonPanel.add(hitButton);
+    stayButton.setFocusable(true);
+    stayButton.addActionListener(null);
+    buttonPanel.add(stayButton);
     
-    JButton standButton = new JButton("Stand");
-    hitButton.addActionListener(null);
-    display.add(standButton);
+    display.add(buttonPanel , BorderLayout.SOUTH);
 }
 
     public static void main(String[] args) {
         JFrame display = windowConfig();
         buttonConfig(display);
-        buttonConfig(display);
-        display.setVisible(true);                             // Visualizes the window (Makes it visable) 
+        display.setVisible(true);                          // Visualizes the window (Makes it visable) 
         
         
         
